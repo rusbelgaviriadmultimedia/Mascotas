fe0ea8c03133a478a737a7f0b6a2369b5fe831a4
rusbelgaviriadmultimedia <kimar041604@gmail.com>
2021-09-25 21:55:38 -0500

    dotnet add reference ../Mascotas.App.Dominio

--- Mascotas.App/Mascotas.App.Persistencia/AppRepositorios/AppContext.cs
+++ Mascotas.App/Mascotas.App.Persistencia/AppRepositorios/AppContext.cs
@@ -1,1 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+
+namespace Mascotas.App.Persistencia
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Persona> Personas {get;set;}
+    }
+}
