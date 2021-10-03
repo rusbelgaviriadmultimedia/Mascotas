1c28f2f4e76c819317c88b4d3755f82cfb30f67d
rusbelgaviriadmultimedia <kimar041604@gmail.com>
2021-10-03 18:58:14 -0500

    Agregando clase Mascota

---
+++ Mascotas.App/Mascotas.App.Dominio/Entidades/Clase_Mascota.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Mascotas.App.Dominio
+{
+    public class Clase_Mascota
+    {
+        public int IdMascota { get;set; }
+        public string NombreMascota { get; set;}
+        public string ColorDeOjosMascota { get;set; }
+        public float EstaturaMascota {get; set;}
+        public string RazaMascota { get; set;} 
+        public string CategoriaMascota { get; set;}        
+    }
+}
