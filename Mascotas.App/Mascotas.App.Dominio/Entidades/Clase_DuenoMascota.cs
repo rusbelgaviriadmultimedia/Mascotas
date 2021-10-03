2110bcfdada81051f23682b3a1a50ede5535fe0a
rusbelgaviriadmultimedia <kimar041604@gmail.com>
2021-10-03 18:55:54 -0500

    Agregando clase duenomascota

---
+++ Mascotas.App/Mascotas.App.Dominio/Entidades/Clase_DuenoMascota.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Mascotas.App.Dominio
+{
+    public class Clase_DuenoMascota
+    {
+        public int IdDuenoMascota { get;set; }
+        public Clase_SolicitudDeVisita SolicitudDeVisita{ get; set;}
+    }
+}
