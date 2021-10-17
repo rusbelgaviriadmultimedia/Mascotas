add61f0c972b9aeb934dab5f712ec43ae3099b57
Rusbel Gaviria <kimar041604@gmail.com>
2021-10-17 17:06:09 -0500

    Conexion base de datos modificandp AppContext // Data Source = ENIGMA; Initial Catalog = AppMascotas; Trusted_Connection=true;

--- Mascotas.App/Mascotas.App.Persistencia/AppData/AppContext.cs
+++ Mascotas.App/Mascotas.App.Persistencia/AppData/AppContext.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Mascotas.App.Dominio.Entidades;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace Mascotas.App.Persistencia
 {
@@ -8,14 +11,14 @@ namespace Mascotas.App.Persistencia
     /// </summary>
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<ClaseMedicoVeterinario> MedicoVeterinario {get;set;}
-        public DbSet<ClaseDuenoMascota> DuenoMascota {get;set;}
+        public DbSet<ClaseMedicoVeterinario> MedicosVeterinarios {get;set;}
+        public DbSet<ClaseDuenoMascota> DuenosMascotas {get;set;}
         ///El tipo 'ClaseTipoAnimal' debe ser un tipo de referencia para poder usarlo como parámetro
         /// ///public DbSet<ClaseTipoAnimal> TipoAnimal {get;set;}
-        public DbSet<ClaseMascota > Mascota {get;set;}
-        public DbSet<ClaseVisitaVeterinaria > VisitaVeterinaria {get;set;}
-        public DbSet<ClaseEmpresa> Empresa {get;set;}
-        public DbSet<ClaseEstadoMascota> estadoMascota {get;set;}
+        public DbSet<ClaseMascota > Mascotas {get;set;}
+        public DbSet<ClaseVisitaVeterinaria > VisitasVeterinarias {get;set;}
+        public DbSet<ClaseEmpresa> Empresas {get;set;}
+        public DbSet<ClaseEstadoMascota> estadosMascotas {get;set;}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
@@ -23,7 +26,7 @@ namespace Mascotas.App.Persistencia
             {
                 ///optionsBuilder.UseSqlServer("Data Source = FINA; Initial Catalog = HospiEnCasa;User Id=FINA\Familia; password=''")
                 //optionsBuilder.UseSqlServer(@"Server = (localdb)\\mssqllocaldb; Database = Mascotas");
-                optionsBuilder.UseSqlServer(" Data Source = FINA; Initial Catalog = Mascotas");
+                optionsBuilder.UseSqlServer(" Data Source = FINA; Initial Catalog = Mascotas; Trusted_Connection=true;");
             }
         }
     }
