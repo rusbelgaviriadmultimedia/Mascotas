196af9cd402ff57db55a6ca250536d5a36b3a324
Rusbel Gaviria <kimar041604@gmail.com>
2021-10-17 11:21:17 -0500

    dotnet ef migrations add Inicial --startup-project ..\HospiEnCasa.App.Consola

---
+++ Mascotas.App/Mascotas.App.Persistencia/Migrations/20211017161939_Inicial.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mascotas.App.Persistencia.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empresa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nit = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RazonSocial = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empresa", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "estadoMascota",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Temperatura = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Peso = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FrecuenciaRespiratoria = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FrecuenciaCardiaca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EstadoDeAnimo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_estadoMascota", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mascota",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ColorDeOjos = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ColorDePiel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Estatura = table.Column<float>(type: "real", nullable: false),
+                    Raza = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    tipoAnimal = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mascota", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MedicoVeterinario",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TarjetaProfesional = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoAnimal = table.Column<int>(type: "int", nullable: false),
+                    Identificacion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumeroTelefono = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicoVeterinario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VisitaVeterinaria",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaVisita = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EstadoMascotaId = table.Column<long>(type: "bigint", nullable: true),
+                    IdVeterinario = table.Column<int>(type: "int", nullable: false),
+                    Recomendaciones = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    medicamentos = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VisitaVeterinaria", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VisitaVeterinaria_estadoMascota_EstadoMascotaId",
+                        column: x => x.EstadoMascotaId,
+                        principalTable: "estadoMascota",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DuenoMascota",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MascotasId = table.Column<long>(type: "bigint", nullable: true),
+                    Identificacion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumeroTelefono = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DuenoMascota", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DuenoMascota_Mascota_MascotasId",
+                        column: x => x.MascotasId,
+                        principalTable: "Mascota",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DuenoMascota_MascotasId",
+                table: "DuenoMascota",
+                column: "MascotasId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VisitaVeterinaria_EstadoMascotaId",
+                table: "VisitaVeterinaria",
+                column: "EstadoMascotaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DuenoMascota");
+
+            migrationBuilder.DropTable(
+                name: "Empresa");
+
+            migrationBuilder.DropTable(
+                name: "MedicoVeterinario");
+
+            migrationBuilder.DropTable(
+                name: "VisitaVeterinaria");
+
+            migrationBuilder.DropTable(
+                name: "Mascota");
+
+            migrationBuilder.DropTable(
+                name: "estadoMascota");
+        }
+    }
+}
