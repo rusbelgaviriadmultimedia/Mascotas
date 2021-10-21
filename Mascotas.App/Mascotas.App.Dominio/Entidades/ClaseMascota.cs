9d4ae4ea354d01a741d9a6079557eb92d6635054
Rusbel Gaviria <kimar041604@gmail.com>
2021-10-21 00:59:32 -0500

    Agregando Entidad ../Mascotas.App.Dominio/Entidades/ClaseMascota.cs

--- Mascotas.App/Mascotas.App.Dominio/Entidades/ClaseMascota.cs
+++ Mascotas.App/Mascotas.App.Dominio/Entidades/ClaseMascota.cs
@@ -8,13 +8,30 @@ namespace Mascotas.App.Dominio.Entidades
 {
     public class ClaseMascota
     {
-        ///[Key]
+        [Key]
+        [Column("IdMascota")]
         public long Id { get;set; }
+        [Required]
+        [Column("NombreMascota")]
+        [StringLength(50,MinimumLength=5)]
         public string Nombre { get; set;}
+        [Required]
+        [Column("ColorDeOjosMascota")]
+        [StringLength(10,MinimumLength=5)]
         public string ColorDeOjos { get;set; }
+        [Required]
+        [Column("ColorDePielMascota")]
+        [StringLength(10,MinimumLength=5)]
         public string ColorDePiel { get;set; }
+        [Required]
+        [Column("EstaturaMascota")]
+        [StringLength(10,MinimumLength=3)]
         public float Estatura{get; set;}
+        [Required]
+        [Column("RazaMascota")]
+        [StringLength(10,MinimumLength=3)]
         public string Raza { get; set;} 
+        [ForeignKey("ClaseTipoAnimal")]
         public ClaseTipoAnimal tipoAnimal { get; set;} 
 
         public ClaseMascota(){}  
