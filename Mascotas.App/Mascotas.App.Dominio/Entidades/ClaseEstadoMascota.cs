88e850eafefd4f3b98fb0217af9b1bce0b1de47e
Rusbel Gaviria <kimar041604@gmail.com>
2021-10-21 00:52:02 -0500

    Agregando entidad ../Mascotas.App.Dominio/Entidades/ClaseEstadoMascota.cs

--- Mascotas.App/Mascotas.App.Dominio/Entidades/ClaseEstadoMascota.cs
+++ Mascotas.App/Mascotas.App.Dominio/Entidades/ClaseEstadoMascota.cs
@@ -9,12 +9,27 @@ namespace Mascotas.App.Dominio.Entidades
     public class ClaseEstadoMascota
     {
         ///[ForeignKey(nameof(Id))]
-        ///[Key]
+        [Key]
         public long Id {get; set;}
+         [Required]
+        [Column("TemperaturaMascota")]
+        [StringLength(10,MinimumLength=2)]
         public string Temperatura { get;set; }
+        [Required]
+        [Column("PesoMascota")]
+        [StringLength(10,MinimumLength=2)]
         public string Peso { get;set; }
+        [Required]
+        [Column("FrecuenciaRepiratoriaMascota")]
+        [StringLength(10,MinimumLength=2)]
         public string FrecuenciaRespiratoria { get;set; }
+        [Required]
+        [Column("FrecuenciaCardiacaMascota")]
+        [StringLength(10,MinimumLength=2)]
         public string FrecuenciaCardiaca { get;set; }
+        [Required]
+        [Column("EstadoAnimoMascota")]
+        [StringLength(20,MinimumLength=5)]
         public string EstadoDeAnimo { get;set; }
 
         //Constructor
