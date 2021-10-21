529ce77e767d23fb092d78c92ddea1d714852420
Rusbel Gaviria <kimar041604@gmail.com>
2021-10-20 23:54:04 -0500

    using System.ComponentModel.DataAnnotations; using System.ComponentModel.DataAnnotations.Schema;

--- Mascotas.App/Mascotas.App.Dominio/Entidades/ClaseMascota.cs
+++ Mascotas.App/Mascotas.App.Dominio/Entidades/ClaseMascota.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Mascotas.App.Dominio.Entidades
 {
     public class ClaseMascota
--- Mascotas.App/Mascotas.App.Dominio/Entidades/ClaseTipoAnimal.cs
+++ Mascotas.App/Mascotas.App.Dominio/Entidades/ClaseTipoAnimal.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mascotas.App.Dominio.Entidades
 {
