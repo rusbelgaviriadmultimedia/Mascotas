738c63cf38eaf1022b56214a9f214b1be8801396
Rusbel Gaviria <kimar041604@gmail.com>
2021-10-20 23:24:51 -0500

    renombrando Entidad Empresa

--- Mascotas.App/Mascotas.App.Dominio/Entidades/ClaseEmpresa.cs
+++ Mascotas.App/Mascotas.App.Dominio/Entidades/ClaseEmpresa.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mascotas.App.Dominio.Entidades
 {
@@ -9,6 +11,7 @@ namespace Mascotas.App.Dominio.Entidades
     {
         ///[ForeignKey(nameof(Nit))]
         ///[Key]
+        /// 
         public int Id { get; set;}
         public string Nit { get; set;}
         public string RazonSocial {get;set;}        
