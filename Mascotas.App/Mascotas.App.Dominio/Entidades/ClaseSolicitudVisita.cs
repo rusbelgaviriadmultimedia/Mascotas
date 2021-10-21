7a04673d2f75e7b4781a4ff69773ba5d33c51efa
Rusbel Gaviria <kimar041604@gmail.com>
2021-10-21 01:47:58 -0500

    Agregando Entidad ../Mascotas.App.Dominio/Entidades/ClaseSolicitudVisita.cs

---
+++ Mascotas.App/Mascotas.App.Dominio/Entidades/ClaseSolicitudVisita.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mascotas.App.Dominio.Entidades
+{
+    [Table("SolicitudVisitaDb")]
+    public class ClaseSolicitudVisita
+    {
+        [Key]
+        [Column("IdSolicitudVisita")]
+        public long id { get; set;}
+        [ForeignKey("IdMascota")]
+        public ClaseMascota mascotas { get; set;}
+        [ForeignKey("TarjetaProfesional")]
+        public virtual ClaseMedicoVeterinario medicoVeterinario { get; set; }
+        [ForeignKey("idMascota")]
+        public ClaseTipoAnimal tipoAnimal { get; set; }
+        [ForeignKey("identificacion")]
+        public ClaseDuenoMascota duenoMascota {get; set;}
+
+    }
+}
